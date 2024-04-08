2fc7b60c90ebd54dd9b6894da1d10dc168c57034
ninoaddict <adrilbless37@gmail.com>
2024-04-08 15:53:31 +0700

    feat: add makefile

--- config/Animal.cpp
+++
@@ -1,8 +0,0 @@
-#include "Animal.hpp"
-#include <iostream>
-using namespace std;
-
-Animal::Animal(string type, int w, int id, string code, string name, int price) : GameObject(id,code,name,price) {
-    this->type = type;
-    this->weight_to_harvest = w;
-}
