fd8c9e030a071463d1904b844b0a5a6bad503a15
sotul04 <suthasoma04@gmail.com>
2024-04-09 20:38:13 +0700

    feat: test FileController

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "GameEngine/header/FileController.hpp"
+#include <vector>
+
 using namespace std;
 
 int main() {
     cout << "Halo nama saya adalah adril" << '\n';
+    vector<vector<string>> result = FileController::readFile("../state/placeholder.txt");
+    for (auto i = result.begin(); i != result.end(); i++) {
+        for (auto j = i->begin(); j != i->end(); j ++) {
+            cout << (*j) << " ";
+        }
+        cout << endl;
+    }
+    FileController::write(result, "../state/written.txt");
 }
