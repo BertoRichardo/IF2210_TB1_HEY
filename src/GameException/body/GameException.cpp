20cfd763a25546dc8fa096991479992537c5eb3e
sotul04 <suthasoma04@gmail.com>
2024-04-10 15:57:08 +0700

    feat: add GameException Abstract Class and FeedTypeInvalidException

---
+++ src/GameException/body/GameException.cpp
@@ -0,0 +1,31 @@
+#include "../header/GameException.hpp"
+
+
+/*GameException*/
+GameException::GameException() : message("Opps, terjadi kesalahan")
+{}
+
+GameException::GameException(const string &message_) : message(message_)
+{}
+
+GameException::~GameException()
+{
+    message.clear(); 
+}
+
+void GameException::setMessage(const string& message_)
+{
+    message = message_;
+}
+
+/*FeedTypeInvalidException*/
+FeedTypeInvalidException::FeedTypeInvalidException(const string& animalType_, const string& productType_) : GameException("Jenis Product tidak dapat dimakan")
+{
+    animalType = animalType_;
+    productType = productType_;
+}
+
+void FeedTypeInvalidException::displayMessage() const {
+    cerr << message << endl;
+    cerr << "Hewan bertipe " << animalType << " tidak dapat memakan product bertipe " << productType << endl;
+}
